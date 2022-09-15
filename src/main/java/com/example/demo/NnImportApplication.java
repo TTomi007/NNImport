b4664e149c31c25d373f7471a6be6111606f561e
ttomi007 <samattoth@gmail.com>
2022-09-15 20:46:07 +0200

    Project init

---
+++ src/main/java/com/example/demo/NnImportApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnImportApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NnImportApplication.class, args);
+		System.out.println("Hello NN!");
+	}
+
+}
